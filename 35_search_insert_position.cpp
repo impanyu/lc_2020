652b728d983d8412293798932105933fc5db0661
Yu Pan <impanyu@gmail.com>
2020-09-06 22:22:00 -0500

    280,278,35

---
+++ 35_search_insert_position.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+       int i=0, j=nums.size();
+        while(i<j){
+            int mid=(i+j)>>1;
+            if(nums[mid]>=target) j=mid;
+            else i=mid+1;       
+        }
+        return i;
+    }
+};
